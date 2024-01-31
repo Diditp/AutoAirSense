6e0a3d333d9914f1a7f4ea258d605118f5339528
Made <imadegedeuditayanakusumaputra@gmail.com>
2024-02-01 00:13:37 +0800

    Update ToolInformation.jsx

--- src/components/ToolInformation.jsx
+++ src/components/ToolInformation.jsx
@@ -3,7 +3,7 @@ export default function ({ imgSrc, name, description, imgAlt }) {
     <>
       <div className="bg-white box-shadow w-11/12 md:room-card min-width my-5 mr-5">
         <img className="bg-cover w-full h-64 md:h-1/2" src={imgSrc} alt={imgAlt} />
-        <div className="flex justify-between w w-full p-2 bg-blue-300">
+        <div className="flex justify-between w w-full p-2 bg-blue-200">
           <h4>{name}</h4>
           <button>Turn On</button>
         </div>
