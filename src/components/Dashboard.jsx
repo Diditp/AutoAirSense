39d8b17ff787b8dd990ccc8e7a0249dc7ecb223e
Made <imadegedeuditayanakusumaputra@gmail.com>
2024-02-05 09:36:57 +0800

    - Menambahkan configurasi ke firebase realtime database  - Mengkoneksikan data di realtime Database ke Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,38 +3,111 @@ import RoomStatusCard from "./RoomStatusCard";
 import ToolInformation from "./ToolInformation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import database from "../database/firebase_database";
+import { onValue, ref , update} from "firebase/database";
 
 export default function Dashboard() {
   const [isExpanded, setIsExpanded] = useState(true);
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [isActive, setActive] = useState('active');
 
   useEffect(() => {
-      const sidebar = document.querySelector('.sidebar');
-      const dashboard = document.querySelector('.dashboard');
+      const intervalId = setInterval(() => {
+        setCurrentTime(new Date());
+      }, 1000);
+  
+      return () => clearInterval(intervalId);
+    }, []);
 
-      // Check if elements are found before trying to modify them
-      if (sidebar && dashboard) {
-          sidebar.style.width = isExpanded ? '0%' : '15%';
-          sidebar.style.display = isExpanded ? 'none' : 'block';
-          dashboard.style.width = isExpanded ? '100%' : '85%';
-      } else {
-          console.error("Elements not found.");
-      }
+  useEffect(() => {
+    const sidebar = document.querySelector(".sidebar");
+    const dashboard = document.querySelector(".dashboard");
+
+    // Check if elements are found before trying to modify them
+    if (sidebar && dashboard) {
+      sidebar.style.width = isExpanded ? "0%" : "15%";
+      sidebar.style.display = isExpanded ? "none" : "block";
+      dashboard.style.width = isExpanded ? "100%" : "85%";
+    } else {
+      console.error("Elements not found.");
+    }
   }, [isExpanded]);
 
   const handleWidthClick = () => {
-      setIsExpanded(!isExpanded);
+    setIsExpanded(!isExpanded);
   };
+
+  let coValue = null;
+  let co2Value = null;
+  let vocValue = null;
+  let pmValue = null;
+  let suhu = null;
+  let kelembapan = null;
+
+  const writeDataToFirebase = () => {
+    // Menggunakan metode push() untuk menambahkan data baru dengan kunci unik
+      update(ref(database, 'dataLogger/dataSensor'), {
+          coValue: 1,
+          co2Value: 1,
+          vocValue: 1,
+          pmValue: 1,
+          suhu: 1,
+          kelembapan: 1,
+          waktuSekarang: currentTime.toLocaleTimeString(),
+      });
+  };
+
+  writeDataToFirebase();
+
+  // Menggunakan metode on() untuk mendengarkan perubahan pada data di Firebase
+  const starCountRef = ref(database, "dataLogger/dataSensor");
+  onValue(starCountRef, (snapshot) => {
+      coValue = snapshot.val().coValue;
+      co2Value = snapshot.val().co2Value;
+      vocValue = snapshot.val().vocValue;
+      pmValue = snapshot.val().pmValue;
+      suhu = snapshot.val().kelembapan;
+      kelembapan = snapshot.val().kelembapan;
+    });
+
+    const [airQualityLabel, setAirQualityLabel] = useState("Buruk");
+
+    const determineAirQuality = () => {
+        console.log("Data Sensor:");
+        console.log("PM Value:", pmValue, pmValue < 50);
+        console.log("CO Value:", coValue, coValue < 2);
+        console.log("CO2 Value:", co2Value, co2Value < 600);
+        console.log("VOC Value:", vocValue, vocValue < 2);
+        console.log("Suhu:", suhu, suhu >= 20 && suhu <= 25);
+        console.log("Kelembapan:", kelembapan, kelembapan >= 30 && kelembapan <= 50);
+      if (pmValue < 50 && coValue < 2 && co2Value < 600 && vocValue < 2 && suhu <= 25 && kelembapan <= 50) {
+        setAirQualityLabel("Baik");
+      } else if ((pmValue >= 50 && pmValue <= 100) || (coValue >= 2 && coValue <= 9) || (co2Value >= 600 && co2Value <= 1000) || (vocValue >= 3 && vocValue <= 6) || (suhu < 27 || suhu > 30) || (kelembapan < 30 || kelembapan > 50)) {
+        setAirQualityLabel("Sedang");
+      } else {
+        setAirQualityLabel("Buruk");
+      }
+    };
+    
+    useEffect(() => {
+      determineAirQuality();
+    }, [pmValue, coValue, co2Value, vocValue, suhu, kelembapan])
 
   return (
     <>
       <div className="flex-1 dashboard">
         <header className="flex justify-between header-height p-5 bg-white items-center ">
           <div className="flex items-center">
-              <FontAwesomeIcon className="mr-5" icon="bars" size="lg" onClick={handleWidthClick}/>
-              <h1 className="text-2xl md:text-4xl">Dashboard</h1>
+            <FontAwesomeIcon
+              className="mr-5"
+              icon="bars"
+              size="lg"
+              onClick={handleWidthClick}
+            />
+            <h1 className="text-2xl md:text-4xl">Dashboard</h1>
           </div>
           <div className="text-center flex items-center">
-          <span className="mr-2 none sm:block">Logout</span>
+            <span className="mr-2 none sm:block">Logout</span>
             <FontAwesomeIcon icon="right-from-bracket" size="xl" />
           </div>
         </header>
@@ -53,31 +126,37 @@ export default function Dashboard() {
                 <SensorCard
                   type="Debu"
                   sensorType="dust"
-                  data="45 ng/m3"
+                  data={pmValue + " µg/m³"}
                   description="Sensor PM2.5 - Memantau debu halus didalam ruangan ini."
                 />
                 <SensorCard
                   type="CO"
                   sensorType="wind"
-                  data="55 ppm"
+                  data={coValue + " PPM"}
+                  description="Sensor MQ-7 - Mengukur kadar gar CO didalam ruangan ini."
+                />
+                <SensorCard
+                  type="CO2"
+                  sensorType="wind"
+                  data={co2Value + " PPM"}
                   description="Sensor MQ-7 - Mengukur kadar gar CO didalam ruangan ini."
                 />
                 <SensorCard
                   type="VOC"
                   sensorType="wind"
-                  data="55 ppm"
+                  data={vocValue + " PPM"}
                   description="Sensor MQ-135 - Mengukur kadar senyawa organik volatil dan CO2 didalam ruangan ini."
                 />
                 <SensorCard
                   type="Suhu"
                   sensorType="temperature"
-                  data="15 Celsius"
+                  data={suhu + " Celcius"}
                   description="Sensor Dht22 - Mengukur suhu didalam ruangan ini."
                 />
                 <SensorCard
                   type="Kelembapan"
                   sensorType="humidity"
-                  data="15 %"
+                  data={kelembapan + " %"}
                   description="Sensor Dht22 - Mengukur kelembapan didalam ruangan ini."
                 />
               </div>
@@ -90,7 +169,7 @@ export default function Dashboard() {
               <div className="flex flex-col justify-center items-center lg:flex-row lg:items-stretch">
                 <RoomStatusCard
                   status="Baik (0 - 50)"
-                  isActive="Active"
+                  isActive={airQualityLabel}
                   imgSrc="udaraBaik.jpg"
                   desc=" Indeks Standar Pencemar Udara (ISPU) adalah suatu metode
                   yang digunakan untuk mengukur kualitas udara dengan memperhitungkan
---
+++ src/database/firebase_database.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9l109tr2-HylkLkpn85ibNDbdm-OmMoQ",
+    authDomain: "autoairsense.firebaseapp.com",
+    databaseURL: "https://autoairsense-default-rtdb.firebaseio.com",
+    projectId: "autoairsense",
+    storageBucket: "autoairsense.appspot.com",
+    messagingSenderId: "1098068575996",
+    appId: "1:1098068575996:web:27be2ccadbbbcf392ab979",
+    measurementId: "G-1ENW0EZW3V"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+export default database;
