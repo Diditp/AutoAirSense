455a470cdf5d0672f1e970d501b5f815c14696d6
Made Putra <imadegedeuditayanakusumaputra@gmail.com>
2024-07-02 18:57:02 +0800

    Fix/login screen responsive

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -13,10 +13,10 @@ const Login = () => {
 
   const handleLogin = () => {
     if (username === EMAIL && password === PASSWORD) {
-      // Autentikasi berhasil, set state loggedIn menjadi true
+      // Authentication successful, set loggedIn state to true
       setLoggedIn(true);
     } else {
-      setError('Email atau password salah');
+      setError('Email or password is incorrect');
     }
   };
 
@@ -25,14 +25,14 @@ const Login = () => {
       {loggedIn ? (
         <Main />
       ) : (
-        <div className="relative flex items-center justify-center h-screen bg-gray-100">
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
           <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url('background.jpg')` }}></div>
-          <div className="relative bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg p-8 rounded-lg shadow-lg w-1/3 max-w-md">
-            <div className='flex flex-col justify-center items-center'>
-              <h2 className="text-2xl font-bold mb-4 text-center">Login To</h2>
+          <div className="relative bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg p-8 rounded-lg shadow-lg w-full sm:w-1/2 md:w-1/3 max-w-md">
+            <div className='flex flex-col justify-center items-center mb-8'>
+              <h2 className="text-2xl font-bold mb-2 text-center">Login To</h2>
               <h2 className="text-2xl font-bold mb-4 text-center">AutoAirSense</h2>
             </div>
-            {error && <div className="text-red-500 mb-4">{error}</div>}
+            {error && <div className="text-red-500 mb-4 text-center">{error}</div>}
             <div className="mb-4">
               <TextField
                 label="Username"
